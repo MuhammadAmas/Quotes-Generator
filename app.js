b3bd305dae2a951c62c09a9c93e42aaa198eaa68
MuhammadAmas <amaswaseem@gmail.com>
2022-07-30 18:26:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+fetch(`https://type.fit/api/quotes`).
+    then(response => {
+        return response.json()
+    }).
+    then(data => {
+        quotes = data;
+
+        document.querySelector('.quote-generator-btn').addEventListener('click', generateQuote);
+     
+        function generateQuote() {
+            let random = Math.floor(Math.random() * 1500);
+            if (quotes[random].author === null) {
+                quotes[random].author = '-  Anonymous';
+            }
+
+            const quoteDiv = document.getElementById('quote-div');
+            // Create div element
+            const row = document.createElement('div');
+            // Insert cols
+            row.innerHTML = `
+            <div class="quote" id="quote">
+                <p id="text" class="text">${quotes[random].text}</p>
+                <p id="author" class="author">${quotes[random].author}</p>
+            </div>
+            `;
+            console.log('**', quoteDiv)
+            quoteDiv.appendChild(row);
+            // document.getElementById('text').innerHTML = quotes[random].text;
+            // document.getElementById('author').innerHTML = quotes[random].author;
+        }
+    }).
+    catch(err => {
+        console.log(err)
+    });
+
+
+
+// random = Math.floor(Math.random() * 1500);
+    // async function run(){
+    //     const response =  await fetch(`https://type.fit/api/quotes`);
+    //     const jsonResponse = await response.json();
+    // }
+    // run()
+
